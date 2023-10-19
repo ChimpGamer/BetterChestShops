49a1bf922c676e1375a4a2b44a5a8e3316727c09
ChimpGamer <Tim.Cremers@Ziggo.nl>
2023-10-19 22:10:02 +0200

    Oops. build file for paper module was missing

---
+++ paper/build.gradle.kts
@@ -0,0 +1,12 @@
+repositories {
+    maven("https://papermc.io/repo/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+
+    implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-api:2.12.1") { isTransitive = false }
+    implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-core:2.12.1") { isTransitive = false }
+
+    implementation("org.bstats:bstats-bukkit:3.0.2")
+}
