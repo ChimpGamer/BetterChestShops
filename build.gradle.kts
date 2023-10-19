a09454312e6b764b6e4869a9f2d0faebb721609d
ChimpGamer <Tim.Cremers@Ziggo.nl>
2023-10-19 22:17:48 +0200

    Update RyseInventory to fix 1.20.2 title update error

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ subprojects {
         compileOnly("dev.dejvokep:boosted-yaml:1.3.1")
         compileOnly("cloud.commandframework:cloud-paper:1.8.4")
         compileOnly("cloud.commandframework:cloud-minecraft-extras:1.8.4")
-        implementation("io.github.rysefoxx.inventory:RyseInventory-Plugin:1.6.7")
+        implementation("io.github.rysefoxx.inventory:RyseInventory-Plugin:1.6.8")
         compileOnly("org.jetbrains.exposed:exposed-core:0.43.0") {
             exclude("org.jetbrains.kotlin")
         }
