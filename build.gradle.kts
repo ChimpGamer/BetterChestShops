8e2dae2db492d787abbf021322d4b6e646327374
ChimpGamer <Tim.Cremers@Ziggo.nl>
2025-04-01 21:09:19 +0200

    Bump JDK from 17 to 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,12 +49,12 @@ subprojects {
 
     java {
         toolchain {
-            languageVersion.set(JavaLanguageVersion.of(17))
+            languageVersion.set(JavaLanguageVersion.of(21))
         }
     }
 
     kotlin {
-        jvmToolchain(17)
+        jvmToolchain(21)
     }
 
     tasks {
